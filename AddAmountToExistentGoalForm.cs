3e899ca39ed3b0acb0953c4042b94b9248138fe7
Bita121 <francu352000@gmail.com>
2023-05-27 20:20:01 +0300

    FinalVersion

--- AddAmountToExistentGoalForm.cs
+++ AddAmountToExistentGoalForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -39,16 +40,88 @@ namespace BudgetManager
 
     return -1; // Goal not found
 }
+        private bool GoalExists(Goals goal)
+        {
+            // Check if the goal already exists in the database
+            using (var connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+            {
+                connection.Open();
 
+                using (var command = new SQLiteCommand("SELECT COUNT(*) FROM Goals WHERE goal_name = @GoalName", connection))
+                {
+                    command.Parameters.AddWithValue("@GoalName", goal.GoalName);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    return count > 0;
+                }
+            }
+        }
+        private void UpdateGoalInDatabase(string goalName, decimal amountToAdd)
+        {
+
+            // Update the goal details in the database
+            using (var connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("UPDATE Goals SET amount_now = amount_now + @AmountToAdd WHERE goal_name = @GoalName", connection))
+                {
+                    command.Parameters.AddWithValue("@AmountToAdd", amountToAdd);
+                    command.Parameters.AddWithValue("@GoalName", goalName);
+
+                    command.ExecuteNonQuery();
+                }
+                using (var command = new SQLiteCommand("UPDATE Goals SET goal_progress = (amount_now/target_amount * 100) WHERE goal_name = @GoalName", connection))
+                {
+                    
+                    command.Parameters.AddWithValue("@GoalName", goalName);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        private Goals GetGoalFromDatabase(string goalName)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Goals WHERE goal_name = @GoalName";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@GoalName", goalName);
+
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Goals goal = new Goals();
+                            goal.GoalName = reader.GetString(1);
+                            goal.TargetAmount = reader.GetDecimal(2);
+                            goal.AmountNow = reader.GetDecimal(3);
+                            goal.dateDeadline = reader.GetDateTime(4);
+                            goal.AdditionalNotes = reader.GetString(5);
+                            goal.GoalProgress = reader.GetDecimal(6);
+
+                            return goal;
+                        }
+                    }
+                }
+            }
+
+            return null; // Return null if the goal was not found in the database
+        }
         private void AddAmountToGoal_Click(object sender, EventArgs e)
         {
             string goalName = textBoxGoalName.Text; // Get the goal name from a TextBox or any other input control
 
             // Find the goal in the list of goals based on its name
-            Goals goal = GlobalVariables.listGoals.FirstOrDefault(g => g.GoalName == goalName);
+           // Goals goal = GlobalVariables.listGoals.FirstOrDefault(g => g.GoalName == goalName);
 
-            if (goal != null)
+            if (GetGoalFromDatabase(goalName)!=null)
             {
+                Goals goal = GetGoalFromDatabase(goalName);
                 // Validate the input amount
                 if (!decimal.TryParse(textBoxAmount.Text, out decimal amountToAdd) || amountToAdd <= 0)
                 {
@@ -58,21 +131,35 @@ namespace BudgetManager
                 decimal newAmountNow = goal.AmountNow + amountToAdd;
                 if (newAmountNow > goal.TargetAmount)
                 {
-                    MessageBox.Show("The resulting amount exceeds the target amount.\nYou can only add ", "Invalid Operation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("The resulting amount exceeds the target amount.", "Invalid Operation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
+                UpdateGoalInDatabase(goalName, amountToAdd);
+
+                
+
                 // Add the amount to the existing goal
                 goal.AmountNow += amountToAdd;
-                goal.GoalProgress = goal.AmountNow / goal.TargetAmount;
+                goal.GoalProgress = goal.AmountNow / goal.TargetAmount * 100;
                 decimal remainingAmount = goal.TargetAmount - goal.AmountNow;
+
+                if (goal.GoalProgress == 100)
+                {
+                    // Delete the goal from the database
+                    DeleteGoalFromDatabase(goalName);
+
+                    // Show a congratulations message
+                    MessageBox.Show("Congratulations! You completed the goal: " + goalName, "Goal Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
 
 
                 // Get the active workbook
                 Excel.Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
 
                 // Get the Goals sheet
-                Excel.Worksheet goalsWorksheet = workbook.Sheets["Goals"] as Excel.Worksheet;
+
+                Excel.Worksheet goalsWorksheet = GetOrCreateWorksheet(workbook, "Goals");
 
                 // Find the row that corresponds to the goal
                 int goalRow = FindGoalRow(goalsWorksheet, goalName);
@@ -83,17 +170,53 @@ namespace BudgetManager
                     goalsWorksheet.Cells[goalRow, 3].Value = goal.AmountNow;
                     goalsWorksheet.Cells[goalRow, 6].Value = goal.GoalProgress;
 
-                    // Save the workbook
-                    workbook.Save();
+                    
 
                     // Show a success message
-                    MessageBox.Show("Amount added to the goal successfully.\nYou can only add " + remainingAmount + " more", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Amount added to the goal successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }      
             }
             else
                 MessageBox.Show("The goal was not found!\nPlease look in the file and input an existing goal!", "Not found!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        private void DeleteGoalFromDatabase(string goalName)
+        {
+            
 
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+            {
+                connection.Open();
+
+                // Create the DELETE statement
+                string deleteQuery = "DELETE FROM Goals WHERE goal_name = @GoalName";
+
+                using (SQLiteCommand command = new SQLiteCommand(deleteQuery, connection))
+                {
+                    // Add the goal name as a parameter
+                    command.Parameters.AddWithValue("@GoalName", goalName);
+
+                    // Execute the DELETE statement
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        private Excel.Worksheet GetOrCreateWorksheet(Excel.Workbook workbook, string worksheetName)
+        {
+            Excel.Worksheet worksheet = null;
+
+            try
+            {
+                worksheet = workbook.Sheets[worksheetName] as Excel.Worksheet;
+            }
+            catch (Exception)
+            {
+                // Worksheet does not exist, create a new one
+                worksheet = workbook.Sheets.Add(Type.Missing, workbook.Sheets[workbook.Sheets.Count]);
+                worksheet.Name = worksheetName;
+            }
+
+            return worksheet;
+        }
         private void AddAmountToExistentGoalForm_Load(object sender, EventArgs e)
         {
 
--- GoalSettingForm.cs
+++ GoalSettingForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.SQLite;
 
 namespace BudgetManager
 {
@@ -32,7 +33,23 @@ namespace BudgetManager
         {
 
         }
+        private bool GoalExists(Goals goal)
+        {
+            // Check if the goal already exists in the database
+            using (var connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+            {
+                connection.Open();
 
+                using (var command = new SQLiteCommand("SELECT COUNT(*) FROM Goals WHERE goal_name = @GoalName", connection))
+                {
+                    command.Parameters.AddWithValue("@GoalName", goal.GoalName);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    return count > 0;
+                }
+            }
+        }
         private void CreateGoal_Click(object sender, EventArgs e)
         {
                 // Validate input values
@@ -65,43 +82,71 @@ namespace BudgetManager
 
                 // Add the goal to the list of goals in GlobalVariables
                 GlobalVariables.listGoals.Add(goal);
+            if (GoalExists(goal))
+            {
+                MessageBox.Show("Goal already exists in the database.", "Duplicate Goal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Exit the method to prevent further processing
+            }
+            else
+            {
+                using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\Program Files\\SQLiteStudio\\GoalsDB.db;Version=3;"))
+                {
+                    connection.Open();
+                    using (SQLiteCommand command = new SQLiteCommand(connection))
+                    {
+                        command.CommandText = "INSERT INTO Goals (goal_name, target_amount, amount_now, date_deadline, additional_notes, goal_progress) VALUES (@GoalName, @TargetAmount, @AmountNow, @Deadline, @AdditionalNotes, @Progress)";
+                        command.Parameters.AddWithValue("@GoalName", goal.GoalName);
+                        command.Parameters.AddWithValue("@TargetAmount", goal.TargetAmount);
+                        command.Parameters.AddWithValue("@AmountNow", goal.AmountNow);
+                        command.Parameters.AddWithValue("@Deadline", goal.dateDeadline);
+                        command.Parameters.AddWithValue("@AdditionalNotes", goal.AdditionalNotes);
+                        command.Parameters.AddWithValue("@Progress", goal.GoalProgress);
+                        command.ExecuteNonQuery();
+                    }
+                }
 
                 // Get the active workbook
                 Excel.Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
 
                 // Get the Goals sheet or create a new one if it doesn't exist
                 Excel.Worksheet goalsWorksheet = null;
-            try
-            {
-                goalsWorksheet = workbook.Sheets["Goals"] as Excel.Worksheet;
-            }
-            catch (Exception)
-            {
-                // Sheet doesn't exist, create a new one
-            }
+                try
+                {
+                    goalsWorksheet = workbook.Sheets["Goals"] as Excel.Worksheet;
+                }
+                catch (Exception)
+                {
+                    // Sheet doesn't exist, create a new one
+                }
 
-            if (goalsWorksheet == null)
-            {
-                // Create a new worksheet for the goals
-                goalsWorksheet = workbook.Sheets.Add(After: workbook.Sheets[workbook.Sheets.Count]);
-                goalsWorksheet.Name = "Goals";
+                if (goalsWorksheet == null )
+                {
+                    // Create a new worksheet for the goals
+                    goalsWorksheet = workbook.Sheets.Add(After: workbook.Sheets[workbook.Sheets.Count]);
+                    goalsWorksheet.Name = "Goals";
 
-                // Write the header row
-                goalsWorksheet.Cells[1, 1].Value = "Goal Name";
-                goalsWorksheet.Cells[1, 2].Value = "Target Amount";
-                goalsWorksheet.Cells[1, 3].Value = "Amount Now";
-                goalsWorksheet.Cells[1, 4].Value = "Deadline";
-                goalsWorksheet.Cells[1, 5].Value = "Additional Notes";
-                goalsWorksheet.Cells[1, 6].Value = "Progress";
-            }
-            else
-            {
-                // Activate the existing "Goals" sheet
-                goalsWorksheet.Activate();
-            }
+                    // Write the header row
+                    goalsWorksheet.Cells[1, 1].Value = "Goal Name";
+                    goalsWorksheet.Cells[1, 2].Value = "Target Amount";
+                    goalsWorksheet.Cells[1, 3].Value = "Amount Now";
+                    goalsWorksheet.Cells[1, 4].Value = "Deadline";
+                    goalsWorksheet.Cells[1, 5].Value = "Additional Notes";
+                    goalsWorksheet.Cells[1, 6].Value = "Progress(%)";
+                }
+                else
+                {
+                    // Activate the existing "Goals" sheet
+                    goalsWorksheet.Activate();
+                    goalsWorksheet.Cells[1, 1].Value = "Goal Name";
+                    goalsWorksheet.Cells[1, 2].Value = "Target Amount";
+                    goalsWorksheet.Cells[1, 3].Value = "Amount Now";
+                    goalsWorksheet.Cells[1, 4].Value = "Deadline";
+                    goalsWorksheet.Cells[1, 5].Value = "Additional Notes";
+                    goalsWorksheet.Cells[1, 6].Value = "Progress(%)";
+                }
 
-            // Determine the row to start writing the goal details
-            int startRow = goalsWorksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row + 1;
+                // Determine the row to start writing the goal details
+                int startRow = goalsWorksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row + 1;
 
                 // Write the goal details to the worksheet
                 goalsWorksheet.Cells[startRow, 1].Value = goal.GoalName;
@@ -109,20 +154,20 @@ namespace BudgetManager
                 goalsWorksheet.Cells[startRow, 3].Value = goal.AmountNow;
                 goalsWorksheet.Cells[startRow, 4].Value = goal.dateDeadline;
                 goalsWorksheet.Cells[startRow, 5].Value = goal.AdditionalNotes;
-                goalsWorksheet.Cells[startRow, 6].Value = goal.GoalProgress + "%";
+                goalsWorksheet.Cells[startRow, 6].Value = goal.GoalProgress;
 
-            // Save the workbook
-            workbook.Save();
 
-            // Clear the form fields
-            textBoxGoalName.Text = string.Empty;
-            textBoxTargetAmount.Text = string.Empty;
-            textBoxAmountNOw.Text = string.Empty;
-            dateTimePickerTargetDate.Value = DateTime.Now;
-            textBoxAdditionalNotes.Text = string.Empty;
 
-            // Show a success message
-            MessageBox.Show("Goal added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Clear the form fields
+                textBoxGoalName.Text = string.Empty;
+                textBoxTargetAmount.Text = string.Empty;
+                textBoxAmountNOw.Text = string.Empty;
+                dateTimePickerTargetDate.Value = DateTime.Now;
+                textBoxAdditionalNotes.Text = string.Empty;
+
+                // Show a success message
+                MessageBox.Show("Goal added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
             
         }
 
